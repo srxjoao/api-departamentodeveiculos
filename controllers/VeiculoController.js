40b95e21bdca822b42ef03ec28fcef5d2d5d19bf
Joao Pedro <joao.barbosa6@estudante.ifms.edu.br>
2025-04-28 14:35:44 -0400

    Quase entrega

--- controllers/VeiculoController.js
+++ controllers/VeiculoController.js
@@ -1,10 +1,27 @@
 const path = require("path");
+const {PrismaClient} = require("@prisma/client")
+const  prisma = new PrismaClient();
+
+
     class VeiculoController{
         static formCadastro(req,res){
             res.sendFile(path.join(__dirname, "..", "views", "formVeiculo.html"));
         }
-        static cadastro(req, res){}
+        
+        static async cadastro(req, res){
+        const veiculo = await prisma.veiculo.create({
+                data:{
+                    placa: req.body.placa,
+                    modelo: req.body.modelo,
+                    ano: parseInt(req.body.ano),
+                    cor: req.body.cor,
+                },
+            })
+            res.send(`O Veículo foi cadastrado sob o ID ${veiculo.id}`);
+        }
 
+    // .body pegar dados que vieram via post e o query e usado para dados que vieram via get        
         static buscarTodos(req, res){}
     }
+
 module.exports = VeiculoController;
--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get("/", (req,res)=>{
 const veiculoRoutes = require("./routes/veiculoRoutes")
 app.use("/veiculos",veiculoRoutes)
 
-app.listen(8000, (err)=>{
+app.listen(8080, (err)=>{
     if(err){
         console.log("Erro"+ JSON.stringify(err));
     }else{
-        console.log(`Aplicação rodando em localhost:8000`);
+        console.log(`Aplicação rodando em localhost:8080`);
     }
 })
